b95d5afeb72945c5be2df42ef37c1b33891af97b
abbas0001 <84105420+abbas0001@users.noreply.github.com>
2022-02-22 10:10:46 +0330

    Update nas.sh

--- nas.sh
+++ nas.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
 TIME=$(date +%Y-%m-%d-%H:%M:%S)
-HOST='192.168.37.10'
-USER='adeli'
-PASS='adeli@1234'
 FILENAME=$TIME.tar
 BASEDIR=~
 tar cf $BASEDIR/ansible-cisco/$FILENAME $BASEDIR/backup
